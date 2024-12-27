ddabb2bf93d03abf4ae6779d44354e5fdfe8bdc5
inde-web <inde.kim@appknot.com>
2024-12-27 16:02:04 +0900

    feature: integer

---
+++ src/bin/integers.rs
@@ -0,0 +1,20 @@
+
+fn main(){
+    // i8,i16,i32,i64,i128, and isize --> Signed Integer
+    // u8,u16,u32,u64,u128, and usize --> Unsigned 
+
+    // computer architecture에 따른 것
+
+    // isize -> 32bit -> i32
+    // usize -> 64bit -> i64
+
+    let my_number:u8 =100;
+    let my_other_number: i32 = 200;
+
+    // let third_number = my_number + my_other_number;
+    // 다른 타입끼리 더 할 수 없음
+
+    // type inference
+
+
+}
